3285222c3c12ad9ab007948f47a2e0581477d3e5
kuria-k <kelvinmacharia442@gmail.com>
2025-09-26 18:27:57 +0300

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './Home'
+import Login from './Login'
+import {BrowserRouter ,Routes, Route, Router } from 'react-router-dom'
+import Signup from './Signup'
+import MedsInput from './Medsinput'
+import Medication from './Medication'
+import Contact from './Contact'
+import Symptom from './Symptom'
+import Student from './Student'
+import HealthBlogs from './HealthBlogs'
+import Dash from './Dash'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+   const [user, setUser] = useState(null);
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login setUser={setUser} />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/medinputs" element={<MedsInput />} />
+        <Route path="/medsearch" element={<Medication />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/symptom" element={<Symptom />} />
+        <Route path="/student" element={<Student user={user} />} />
+        <Route path='students' element={<Student/>}/>
+        <Route path="/blogs" element={<HealthBlogs />} />
+        <Route path="/dash" element={<Dash />} />
+      </Routes>
+    </BrowserRouter>
+    
+  
+      
+  )
+}
+
+export default App
